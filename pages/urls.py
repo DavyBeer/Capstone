d2df7233c1eb4cb3733af73269aa74785344aca1
Steven <steven1126@swbell.net>
2020-10-21 12:14:50 -0500

    Paths for the categories

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, Cat1PageView, Cat2PageView, Cat3PageView, Cat4PageView, Cat5PageView, BlogDetailView
 
 urlpatterns = [
-    path('about/', AboutPageView.as_view(), name='about'),
+    path('cat1/', Cat1PageView.as_view(), name='cat1'),
+    path('cat2/', Cat2PageView.as_view(), name='cat2'),
+    path('cat3/', Cat3PageView.as_view(), name='cat3'),
+    path('cat4/', Cat4PageView.as_view(), name='cat4'),
+    path('cat5/', Cat5PageView.as_view(), name='cat5'),
+    path('cat1/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', HomePageView.as_view(), name='home'),
 ]
