665292b16377aa31d24a91524e950d03abcdeea3
Steven <steven1126@swbell.net>
2020-10-21 11:36:57 -0500

    Multiple categories

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cat1, Cat2, Cat3, Cat4, Cat5
 
-# Register your models here.
+admin.site.register(Cat1)
+admin.site.register(Cat2)
+admin.site.register(Cat3)
+admin.site.register(Cat4)
+admin.site.register(Cat5)
