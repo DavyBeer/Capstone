cf4c22cce2a708260bdde875302156f9226e62cb
Steven <steven1126@swbell.net>
2020-10-21 12:15:05 -0500

    Views for each category

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,29 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Cat1, Cat2, Cat3, Cat4, Cat5
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
+class Cat1PageView(ListView):
+    model = Cat1
+    template_name = 'list.html'
+
+class Cat2PageView(ListView):
+    model = Cat2
+    template_name = 'list.html'
+
+class Cat3PageView(ListView):
+    model = Cat3
+    template_name = 'list.html'
+
+class Cat4PageView(ListView):
+    model = Cat4
+    template_name = 'list.html'
+
+class Cat5PageView(ListView):
+    model = Cat5
+    template_name = 'list.html'
+
+class BlogDetailView(DetailView):
+    model = Cat1
+    template_name = 'post_detail.html'
