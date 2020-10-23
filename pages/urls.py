8e83384b163e4da581d9da23f5b1f57309adb808
Steven <steven1126@swbell.net>
2020-10-23 11:58:02 -0500

    Link for new post page

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, Cat1PageView, Cat2PageView, Cat3PageView, Cat4PageView, Cat5PageView, Cat1DetailView, SignUpView
+from .views import HomePageView, Cat1PageView, Cat2PageView, Cat3PageView, Cat4PageView, Cat5PageView, Cat1DetailView, SignUpView, BlogCreateView
 
 urlpatterns = [
     path('cat1/', Cat1PageView.as_view(), name='cat1'),
     path('cat1/<int:pk>/', Cat1DetailView.as_view(), name='post_detail'),
+    path('cat1/new/', BlogCreateView.as_view(), name='post_new'),
     path('cat2/', Cat2PageView.as_view(), name='cat2'),
     path('cat3/', Cat3PageView.as_view(), name='cat3'),
     path('cat4/', Cat4PageView.as_view(), name='cat4'),
