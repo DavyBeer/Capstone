c74576d552f420fb300db828860474e8648542a4
Steven <steven1126@swbell.net>
2020-10-23 10:41:26 -0500

    Lazy signup view

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Cat1, Cat2, Cat3, Cat4, Cat5
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -24,6 +27,11 @@ class Cat5PageView(ListView):
     model = Cat5
     template_name = 'list.html'
 
-class BlogDetailView(DetailView):
+class Cat1DetailView(DetailView):
     model = Cat1
     template_name = 'post_detail.html'
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
