40c20696c593a51900f610a6270c16ec6e6d7c2e
Steven <steven1126@swbell.net>
2020-10-23 10:38:48 -0500

    Path for Signup

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import HomePageView, Cat1PageView, Cat2PageView, Cat3PageView, Cat4PageView, Cat5PageView, BlogDetailView
+from .views import HomePageView, Cat1PageView, Cat2PageView, Cat3PageView, Cat4PageView, Cat5PageView, Cat1DetailView, SignUpView
 
 urlpatterns = [
     path('cat1/', Cat1PageView.as_view(), name='cat1'),
+    path('cat1/<int:pk>/', Cat1DetailView.as_view(), name='post_detail'),
     path('cat2/', Cat2PageView.as_view(), name='cat2'),
     path('cat3/', Cat3PageView.as_view(), name='cat3'),
     path('cat4/', Cat4PageView.as_view(), name='cat4'),
     path('cat5/', Cat5PageView.as_view(), name='cat5'),
-    path('cat1/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('', HomePageView.as_view(), name='home'),
 ]
