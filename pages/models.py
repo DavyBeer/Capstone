3d714afb887fc8e0612856be93fcb774568049af
Steven <steven1126@swbell.net>
2020-11-06 19:08:58 -0500

    Comment Model

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class Cat1(models.Model):
     title = models.CharField(max_length=200)
@@ -15,6 +16,23 @@ class Cat1(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.pk)])
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Cat1,
+        on_delete=models.CASCADE,
+        related_name='comments',)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+    
+    def get_absolute_url(self):
+        return reverse('list')
 
 class Cat2(models.Model):
     title = models.CharField(max_length=200)
