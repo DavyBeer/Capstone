a1de55fabdaa3add930a63daddb2db321bd86927
Steven <steven1126@swbell.net>
2020-11-06 19:08:41 -0500

    Comments

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Cat1, Cat2, Cat3, Cat4, Cat5
+from .models import Cat1, Cat2, Cat3, Cat4, Cat5, Comment
 
-admin.site.register(Cat1)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+    CommentInline,
+]
+
+admin.site.register(Cat1, ArticleAdmin)
+admin.site.register(Comment)
 admin.site.register(Cat2)
 admin.site.register(Cat3)
 admin.site.register(Cat4)
