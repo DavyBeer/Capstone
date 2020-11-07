ba5bce5d68d73ba7d1c532c4cf6477a8ffa6f6a1
Steven <steven1126@swbell.net>
2020-11-07 10:39:36 -0500

    Comments for each category

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Cat1, Cat2, Cat3, Cat4, Cat5, Comment
+from .models import Cat1, Cat2, Cat3, Cat4, Cat5, Comment, Comment2, Comment3, Comment4, Comment5
 
 class CommentInline(admin.TabularInline):
     model = Comment
+    extra = 0
+
+class CommentInline2(admin.TabularInline):
+    model = Comment2
+    extra = 0
+
+class CommentInline3(admin.TabularInline):
+    model = Comment3
+    extra = 0
+
+class CommentInline4(admin.TabularInline):
+    model = Comment4
+    extra = 0
+
+class CommentInline5(admin.TabularInline):
+    model = Comment5
     extra = 0
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -10,9 +26,33 @@ class ArticleAdmin(admin.ModelAdmin):
     CommentInline,
 ]
 
+class ArticleAdmin2(admin.ModelAdmin):
+    inlines = [
+    CommentInline2,
+]
+
+class ArticleAdmin3(admin.ModelAdmin):
+    inlines = [
+    CommentInline3,
+]
+
+class ArticleAdmin4(admin.ModelAdmin):
+    inlines = [
+    CommentInline4,
+]
+
+class ArticleAdmin5(admin.ModelAdmin):
+    inlines = [
+    CommentInline5,
+]
+
 admin.site.register(Cat1, ArticleAdmin)
 admin.site.register(Comment)
-admin.site.register(Cat2)
-admin.site.register(Cat3)
-admin.site.register(Cat4)
-admin.site.register(Cat5)
+admin.site.register(Cat2, ArticleAdmin2)
+admin.site.register(Comment2)
+admin.site.register(Cat3, ArticleAdmin3)
+admin.site.register(Comment3)
+admin.site.register(Cat4, ArticleAdmin4)
+admin.site.register(Comment4)
+admin.site.register(Cat5, ArticleAdmin5)
+admin.site.register(Comment5)
